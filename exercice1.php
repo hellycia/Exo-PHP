1f21d6ecfaebefc57da82dc5e8671fe4b103d5cd
hellycia <wattiaux.helene@hotmail.fr>
2020-07-20 15:55:43 +0200

    Revert "exo 1"
    This reverts commit dd58519b01f55f27b4667f933839d0d27d7f946a.

--- exercice1.php
+++ exercice1.php
@@ -1,30 +1,21 @@
 <?php
 class exercice1
 {
-
-    function today(){
-        echo('Aujourdjhui nous sommes le ' . date('l jS \of F Y h:i:s'));
+function friday($date){
+    $date = date("l", $date);
+    if($date == "Monday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }elseif($date == "Tuesday") {
+        printf('Aujourd hui j ai piscine');
+    }elseif($date == "Wednesday") {
+        printf('Jour des enfants');
+    }elseif($date == "Thursday") {
+        printf('On sort boire un verre?');
+    }elseif($date == "Friday") {
+        printf('Presque en weekend!');
+    }elseif($date == "Saturday") {
+        printf('Qui fait du PHP le weekend?');
+    }elseif($date == "Sunday") {
+        printf('Pas dispo');
     }
-
-    function friday(){
-
-}
-
-    function week($date){
-        $date = date("l", $date);
-        if($date == "Monday") {
-            printf('Cest parti pour une nouvelle semaine');
-        }elseif($date == "Tuesday") {
-            printf('Aujourd hui j ai piscine');
-        }elseif($date == "Wednesday") {
-            printf('Jour des enfants');
-        }elseif($date == "Thursday") {
-            printf('On sort boire un verre?');
-        }elseif($date == "Friday") {
-            printf('Presque en weekend!');
-        }elseif($date == "Saturday") {
-            printf('Qui fait du PHP le weekend?');
-        }elseif($date == "Sunday") {
-            printf('Pas dispo');
-        }
 }
