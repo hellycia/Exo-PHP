d392365ef5d413f5a562e94f696612b71d711182
hellycia <wattiaux.helene@hotmail.fr>
2020-07-20 15:36:17 +0200

    exo 1

---
+++ exercice1.php
@@ -0,0 +1,24 @@
+<?php
+class exercice1
+{
+function friday($date){
+    $date = date("l", $date);
+    if($date == "Monday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+    if($date == "Tuesday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+    if($date == "Wednesday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+    if($date == "Thursday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+    if($date == "Friday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+    if($date == "Saturday") {
+        printf('Cest parti pour une nouvelle semaine');
+    }
+}
