ed6eda1203c72e92a03b91921e72393b800678a4
hellycia <wattiaux.helene@hotmail.fr>
2020-07-23 10:43:16 +0200

    Refacto index

--- exercice4.php
+++ exercice4.php
@@ -1,41 +1,41 @@
 <?php
 
-    $nombre1 =  readline("Le premier nombre? ");
-    $nombre2 = readline("Le deuxième nombre? ");
+$nombre1 =  readline("Le premier nombre? ");
+$nombre2 = readline("Le deuxième nombre? ");
 
-    $nombre1 = intval($nombre1);
-    $nombre2 = intval($nombre2);
+$nombre1 = intval($nombre1);
+$nombre2 = intval($nombre2);
 
-    //$nombre1 = intval(readline("Le premier nombre? "));
+//$nombre1 = intval(readline("Le premier nombre? "));
 
-    if($nombre1 === 0 || $nombre2 === 0){
-        exit("Vous devez rentrer un entier positif");
-    }
+if($nombre1 === 0 || $nombre2 === 0){
+    exit("Vous devez rentrer un entier positif");
+}
 
-    $resultats = [];
-    $resultats['+'] = additionner($nombre1, $nombre2);
-    $resultats['*'] = multiplier($nombre1, $nombre2);
-    $resultats['/'] = diviser($nombre1, $nombre2);
-    $resultats['%'] = modulo($nombre1, $nombre2);
+$resultats = [];
+$resultats['additionner'] = additionner($nombre1, $nombre2);
+$resultats['multiplier'] = multiplier($nombre1, $nombre2);
+$resultats['diviser'] = diviser($nombre1, $nombre2);
+$resultats['modulo'] = modulo($nombre1, $nombre2);
 
-    foreach($resultats as $index => $resultat){
-        echo($nombre1 . $index . $nombre2 . '=' . $resultat . "\n");
-    }
+echo('Nombre1 = ' . $nombre1 . ' | Nombre2 = ' . $nombre2 . "\n");
+foreach($resultats as $index => $resultat){
+    echo($index . ' : ' . $resultat . "\n");
+}
 
+function additionner(int $a, int $b) : int {
+    return $a + $b;
+}
 
-    function additionner(int $a, int $b) : int {
-        return $a + $b;
-    }
+function multiplier(int $a, int $b) : int {
+    return $a * $b;
+}
 
-    function multiplier(int $a, int $b) : int {
-        return $a * $b;
-    }
+function diviser(int $a, int $b) : float {
+    return $a / $b;
+}
 
-    function diviser(int $a, int $b) : float {
-        return $a / $b;
-    }
-
-    function modulo(int $a, int $b) : int {
-        return $a % $b;
-    }
+function modulo(int $a, int $b) : int {
+    return $a % $b;
+}
 ?>
