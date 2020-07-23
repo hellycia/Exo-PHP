02ba4611d94b2845a406b395ed6eaaecf764e3a4
hellycia <wattiaux.helene@hotmail.fr>
2020-07-23 11:04:38 +0200

    Exercice 4C finish

--- exercice4.php
+++ exercice4.php
@@ -1,4 +1,5 @@
 <?php
+    //$nombre1 = intval(readline("Le premier nombre? "));
 
 $nombre1 =  readline("Le premier nombre? ");
 $nombre2 = readline("Le deuxième nombre? ");
@@ -8,14 +9,18 @@ $nombre2 = intval($nombre2);
 
 //$nombre1 = intval(readline("Le premier nombre? "));
 
-if(
-    $nombre1 <= 10 ||
-    $nombre1 >= 100 ||
-    $nombre2 > $nombre1 ||
-    $nombre2 === 0
-) {
-    exit("Saisie invalide");
-}
+do {
+    $nombre1 =  readline("Le premier nombre? ");
+    $nombre2 = readline("Le deuxième nombre? ");
+    $nombre1 = intval($nombre1);
+    $nombre2 = intval($nombre2);
+
+    $truc = (condition($nombre1, $nombre2) == true);
+
+    if($truc) {
+        echo("Le chiffre nest pas bon, ");
+    }
+} while($truc);
 
 $resultats = [];
 $resultats['additionner'] = additionner($nombre1, $nombre2);
@@ -42,5 +47,17 @@ function diviser(int $a, int $b) : float {
 
 function modulo(int $a, int $b) : int {
     return $a % $b;
+}
+
+function condition(int $nombre1, int $nombre2) : bool {
+    $nombre1EstInferieurOuEgalA10 = $nombre1 <= 10;
+    $nombre1EstSuperieurOuEgalA100 = $nombre1 >= 100;
+    $nombre2EstSuperieurANombre1 = $nombre2 > $nombre1;
+    $nombre2EstStrictementEgalA0 = $nombre2 === 0;
+
+    return  $nombre1EstInferieurOuEgalA10 or
+            $nombre1EstSuperieurOuEgalA100 or
+            $nombre2EstSuperieurANombre1 or
+            $nombre2EstStrictementEgalA0;
 }
 ?>
